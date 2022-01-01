efed33923c868688ee87ec48c13b0923f2661d80
Vorobev Pavel <work.pas.vorobiov@gmail.com>
2022-01-01 18:03:09 +0300

    Доработка метода utility.PrettyPrintStringsSlice
    - Исправлены ишибки в выведении элементов
    - Теперь отображение идёт в кавычках
    - Тесты стали более понятными

--- utility/slice.go
+++ utility/slice.go
@@ -17,7 +17,19 @@ func StringIsIn(value string, slice []string) bool {
 func PrettyPrintStringsSlice(slice []string) string {
 	var result = "["
 	for _, item := range slice {
-		result += fmt.Sprint(item) + " ,"
+		result += "\"" + fmt.Sprint(item) + "\"" + ","
 	}
-	return result[:len(result)-3] + "]"
+	return result[:len(result)-1] + "]"
+}
+
+func StringSliceEqual(slice []string, other []string) bool {
+	if len(slice) != len(other) {
+		return false
+	}
+	for index := range slice {
+		if slice[index] != other[index] {
+			return false
+		}
+	}
+	return true
 }
